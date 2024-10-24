722a31d9bd36c222ec2f0fc5baf0332fcd0f87fd
JoaoBagvanji <jbagvanji54@gmail.com>
2024-10-24 15:23:16 -0300

    Feedback commit

---
+++ src/Components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -0,0 +1,46 @@
+import { FeedbackType, feedBackTypes } from "..";
+import { CloseButton } from "../../CloseButton";
+
+interface  FeedbackTypeStepProps{
+    onFeedbackTypeChanged: (type: FeedbackType) => void;
+}
+
+export function FeedbackTypeStep({onFeedbackTypeChanged}: FeedbackTypeStepProps){
+    return(
+        <>
+            <header >
+
+                <span className="text-xl leading-6">
+                    Deixe seu feedback
+                </span>
+
+                <CloseButton />
+            </header>
+
+
+
+            <div className="flex py-8 gap-2 w-full">
+                { Object.entries(feedBackTypes).map(([key, value])=>{
+                    return(
+                        <button
+                            key={key} 
+                            className="bg-zinc-800 rounded-lg py-12 w-24 flex-1 flex flex-col items-center
+                            gap-2 border-2 border-transparent hover:border-brand-500
+                            focus:border-brand-500 focus:outline-none justify-center "
+                            type="button"
+                            onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
+                        >
+
+                            <img  src={value.image.source} alt={value.image.alt} />
+
+                            <span>
+                                {value.title}
+                            </span>
+
+                        </button>
+                    );
+                })}
+            </div>
+        </>
+    )
+}
